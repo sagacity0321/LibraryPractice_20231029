fb5521274b7ee052d21833e1c912298fc0161f98
sagacity0321 <citronica0321@gmail.com>
2023-10-29 15:06:34 +0900

    이미지 화면에서 줌인아웃 기능

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,14 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
+        maven { url = uri("https://www.jitpack.io") }    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://www.jitpack.io") }
     }
 }
 
